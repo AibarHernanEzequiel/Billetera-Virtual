52ba28225c039a0b779d7cc9c340af94159bff4f
Aibar Hernan Ezequiel <ezequiel@Debian.168.1.4>
2022-05-09 11:37:48 -0300

    Realiza test de la clase register controller

--- src/main/java/com/aibar/controllers/RegisterController.java
+++ src/main/java/com/aibar/controllers/RegisterController.java
@@ -23,43 +23,44 @@ import com.aibar.exceptions.PasswordNotEquals;
 @RestController
 public class RegisterController {
 
-	@PostMapping(value = "validar-formulario")
-	public ResponseEntity<Map<String, Object>> validarFormulario(@RequestBody RegisterData registerData) {
-		Map<String, Object> map = new HashMap<>();
-		HttpStatus status = HttpStatus.CREATED;
-		try {
-			if (isValidEmail(registerData) && isValidPassword(registerData)) {
-				map.put("Mensaje", "Te registraste correctamente");
-			}
-		} catch (InvalidEmailException e) {
-			map.put("Error", "P-500");
-			map.put("Mensaje", "Ingresaste un email invalido");
-			status = HttpStatus.INTERNAL_SERVER_ERROR;
-		} catch (PasswordNotEquals e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NotContainsCapitalLetters e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NotContainsNunbers e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidLength e) {
-			map.put("Error", "P-501");
-			map.put("Mensaje", "Ingresaste una clave con longitud invalida, debe contener al menos 8 caracteres");
-			status = HttpStatus.INTERNAL_SERVER_ERROR;
-		}
+    @PostMapping(value = "validar-formulario")
+    public ResponseEntity<Map<String, Object>> validarFormulario(@RequestBody RegisterData registerData) {
+        Map<String, Object> map = new HashMap<>();
+        HttpStatus status = HttpStatus.CREATED;
+        try {
+            if (isValidEmail(registerData) && isValidPassword(registerData)) {
+                map.put("Mensaje", "Te registraste correctamente");
+            }
+        } catch (InvalidEmailException e) {
+            status = getHttpStatus(map, "P-500", "Ingresaste un email invalido");
 
-		return new ResponseEntity<Map<String, Object>>(map, status);
-	}
+        } catch (PasswordNotEquals e) {
+            status = getHttpStatus(map, "P-502", "Las claves ingresadas no coinciden");
 
-	private boolean isValidPassword(RegisterData registerData)
-			throws PasswordNotEquals, NotContainsCapitalLetters, NotContainsNunbers, InvalidLength {
-		return PasswordValidator.validatePassword(registerData.getPassword(), registerData.getRepeatPassword());
-	}
+        } catch (NotContainsCapitalLetters e) {
+            status = getHttpStatus(map, "P-503", "La clave debe contener una mayuscula");
 
-	private boolean isValidEmail(RegisterData registerData) throws InvalidEmailException {
-		return EmailValidator.validateEmail(registerData.getEmail());
-	}
+        } catch (NotContainsNunbers e) {
+            status = getHttpStatus(map, "P-504", "La clave debe contener al menos un numero");
 
+        } catch (InvalidLength e) {
+            status = getHttpStatus(map, "P-501", "Ingresaste una clave con longitud invalida, debe contener al menos 8 caracteres");
+        }
+
+        return new ResponseEntity<>(map, status);
+    }
+
+    private boolean isValidPassword(RegisterData registerData) throws PasswordNotEquals, NotContainsCapitalLetters, NotContainsNunbers, InvalidLength {
+        return PasswordValidator.validatePassword(registerData.getPassword(), registerData.getRepeatPassword());
+    }
+
+    private boolean isValidEmail(RegisterData registerData) throws InvalidEmailException {
+        return EmailValidator.validateEmail(registerData.getEmail());
+    }
+
+    private HttpStatus getHttpStatus(Map<String, Object> map, String codeError, String mensaje) {
+        map.put("Error", codeError);
+        map.put("Mensaje", mensaje);
+        return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
 }
--- src/test/java/com/aibar/controllers/RegisterControllerTest.java
+++ src/test/java/com/aibar/controllers/RegisterControllerTest.java
@@ -30,64 +30,90 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @WebMvcTest(controllers = RegisterController.class)
 public class RegisterControllerTest {
 
-	private RegisterController controller = new RegisterController();
+    private RegisterController controller = new RegisterController();
 
-	@Test
-	void queCuandoUnUsuarioQuieraRegistrarseValideCorrectamente() {
-		RegisterData data = givenQueUnUsuarioIngresaSusDatosEnElFormularioDeRegistro();
-		ResponseEntity<Map<String, Object>> response = whenEnviaElFormulario(data);
-		thenDeberiaEnviarUnMensajeYElStatusCodeDebeSerCreated(response);
-	}
+    @Test
+    void queCuandoUnUsuarioQuieraRegistrarseValideCorrectamente() {
+        RegisterData data = givenQueUnUsuarioIngresaSusDatosEnElFormularioDeRegistro();
+        ResponseEntity<Map<String, Object>> response = whenEnviaElFormulario(data);
+        thenDeberiaEnviarUnMensajeYElStatusCodeDebeSerCreated(response);
+    }
 
-	private void thenDeberiaEnviarUnMensajeYElStatusCodeDebeSerCreated(ResponseEntity<Map<String, Object>> response) {
-		assertThat(response.getBody().get("Mensaje")).isEqualTo("Te registraste correctamente");
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-	}
+    private void thenDeberiaEnviarUnMensajeYElStatusCodeDebeSerCreated(ResponseEntity<Map<String, Object>> response) {
+        assertThat(response.getBody().get("Mensaje")).isEqualTo("Te registraste correctamente");
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
 
-	private ResponseEntity<Map<String, Object>> whenEnviaElFormulario(RegisterData data) {
-		return controller.validarFormulario(data);
-	}
+    private ResponseEntity<Map<String, Object>> whenEnviaElFormulario(RegisterData data) {
+        return controller.validarFormulario(data);
+    }
 
-	private RegisterData givenQueUnUsuarioIngresaSusDatosEnElFormularioDeRegistro() {
-		return getRegisterData("Ezequiel", "Aibar", "ezequiel@gmail.com", "A1234567", "A1234567");
-	}
+    private RegisterData givenQueUnUsuarioIngresaSusDatosEnElFormularioDeRegistro() {
+        return getRegisterData("Ezequiel", "Aibar", "ezequiel@gmail.com", "A1234567", "A1234567");
+    }
 
-	private RegisterData getRegisterData(String nombre, String apellido, String email, String clave,
-			String repiteClave) {
-		return new RegisterData(nombre, apellido, email, clave, repiteClave);
-	}
+    private RegisterData getRegisterData(String nombre, String apellido, String email, String clave, String repiteClave) {
+        return new RegisterData(nombre, apellido, email, clave, repiteClave);
+    }
 
-	@Test
-	void queUnUsuarioAlIngresarUnEmailInvalidoNoPuedaRegistrarse() {
-		RegisterData dataWithInvalidEmailData = givenQueUnUsuarioIngresaUnEmailInvalido();
-		ResponseEntity<Map<String, Object>> response = whenEnviaElFormulario(dataWithInvalidEmailData);
-		thenElRegistroFallaYDevuelveUnStatus500(response, "P-500", "Ingresaste un email invalido");
-	}
+    @Test
+    void queUnUsuarioAlIngresarUnEmailInvalidoNoPuedaRegistrarse() {
+        RegisterData dataWithInvalidEmailData = givenQueUnUsuarioIngresaUnEmailInvalido();
+        ResponseEntity<Map<String, Object>> response = whenEnviaElFormulario(dataWithInvalidEmailData);
+        thenElRegistroFallaYDevuelveUnStatus500(response, "P-500", "Ingresaste un email invalido");
+    }
 
-	private RegisterData givenQueUnUsuarioIngresaUnEmailInvalido() {
-		return getRegisterData("Ezequiel", "Aibar", "ezequiel.com", "1234", "1234");
-	}
+    private RegisterData givenQueUnUsuarioIngresaUnEmailInvalido() {
+        return getRegisterData("Ezequiel", "Aibar", "ezequiel.com", "1234", "1234");
+    }
 
-	private void thenElRegistroFallaYDevuelveUnStatus500(ResponseEntity<Map<String, Object>> response, String error,
-			String mensaje) {
-		assertThat(response.getBody().get("Error")).isEqualTo(error);
-		assertThat(response.getBody().get("Mensaje")).isEqualTo(mensaje);
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    private void thenElRegistroFallaYDevuelveUnStatus500(ResponseEntity<Map<String, Object>> response, String error, String mensaje) {
+        assertThat(response.getBody().get("Error")).isEqualTo(error);
+        assertThat(response.getBody().get("Mensaje")).isEqualTo(mensaje);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-	@Test
-	void queUnUsuarioCuandoIngreseUnaClaveInvalidaNoPuedaRegistrase() {
-		RegisterData dataWithInvalidPassword = givenQueUnUsuarioIngresaUnaClaveInvalida();
-		ResponseEntity<Map<String, Object>> response = whenEnviaElFormulario(dataWithInvalidPassword);
-		thenElRegistroFallaYDevuelveUnStatus500(response, "P-501", "Ingresaste una clave invalida");
-	}
+    @Test
+    void queUnUsuarioCuandoIngreseUnaClaveInvalidaNoPuedaRegistrase() {
+        RegisterData dataWithInvalidPassword = givenQueUnUsuarioIngresaUnaClaveInvalida();
+        ResponseEntity<Map<String, Object>> response = whenEnviaElFormulario(dataWithInvalidPassword);
+        thenElRegistroFallaYDevuelveUnStatus500(response, "P-501", "Ingresaste una clave con longitud invalida, debe contener al menos 8 caracteres");
+    }
 
-	private RegisterData givenQueUnUsuarioIngresaUnaClaveInvalida() {
-		return getRegisterData("Ezequiel", "Aibar", "ezequiel@gmail.com", "A123456", "A123456");
-	}
-	
-	@Test
-	void test() {
-		
-	}
+    private RegisterData givenQueUnUsuarioIngresaUnaClaveInvalida() {
+        return getRegisterData("Ezequiel", "Aibar", "ezequiel@gmail.com", "A123456", "A123456");
+    }
+
+    @Test
+    void queUnUsuarioAlIngresarClavesDistintasNoPuedaRegistrarse() {
+        RegisterData dataWithNotEqualsPassword = givenQueUnUsuarioIngresaClavesDistintas();
+        ResponseEntity<Map<String, Object>> response = whenEnviaElFormulario(dataWithNotEqualsPassword);
+        thenElRegistroFallaYDevuelveUnStatus500(response, "P-502", "Las claves ingresadas no coinciden");
+    }
+
+    private RegisterData givenQueUnUsuarioIngresaClavesDistintas() {
+        return getRegisterData("Ezequiel", "Aibar", "ezequiel@gmail.com", "A1234567", "A123456");
+    }
+
+    @Test
+    void queUnUsuaarioAlNoIngresarUnaClaveConMayusuclasNoPuedaRegistrarse() {
+        RegisterData passwordWithOutCapitalLetter = givenQueUnUsuarioINgresaUnaClaveSinMayuscla();
+        ResponseEntity<Map<String, Object>> response = whenEnviaElFormulario(passwordWithOutCapitalLetter);
+        thenElRegistroFallaYDevuelveUnStatus500(response, "P-503", "La clave debe contener una mayuscula");
+    }
+
+    private RegisterData givenQueUnUsuarioINgresaUnaClaveSinMayuscla() {
+        return getRegisterData("Ezequiel", "Aibar", "ezequiel@gmail.com", "a1234567", "a1234567");
+    }
+
+    @Test
+    void queUnUsuarioAlNoIngresarNumerosEnSuClaveNoPuedeRegistrarse() {
+        RegisterData passwordWithOutNumbers = givenQueUnUsuarioIngresaUnaClaveSinNumeros();
+        ResponseEntity<Map<String, Object>> response = whenEnviaElFormulario(passwordWithOutNumbers);
+        thenElRegistroFallaYDevuelveUnStatus500(response, "P-504", "La clave debe contener al menos un numero");
+    }
+
+    private RegisterData givenQueUnUsuarioIngresaUnaClaveSinNumeros() {
+        return getRegisterData("Ezequiel", "Aibar", "ezequiel@gmail.com", "Qwertyui", "Qwertyui");
+    }
 }
